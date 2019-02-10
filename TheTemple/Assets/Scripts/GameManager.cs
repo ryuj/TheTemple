263342e2119fba239208d420ac4a965fcda18ef8
JinushiRyuichi <ryuichi.jinushi@drecom.co.jp>
2019-02-10 22:47:07 +0900

    ポイント加算ができるようになった。

---
+++ TheTemple/Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private const int MAX_ORB = 10;
+
+    public GameObject orbPrefab;
+    public GameObject canvasGame;
+    public GameObject textScore;
+
+    private int score = 0;
+    private int nextScore = 100;
+
+
+    void Start()
+    {
+        for (int i = 0; i < MAX_ORB; ++i)
+        {
+            CreateOrb();
+        }
+
+        RefreshScoreText();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void CreateOrb()
+    {
+        var orb = Instantiate(orbPrefab);
+        orb.transform.SetParent(canvasGame.transform, false);
+        orb.transform.localPosition = new Vector3(
+            UnityEngine.Random.Range(-300, 300),
+            UnityEngine.Random.Range(-140, -500));
+    }
+
+    public void GetOrb()
+    {
+        score += 1;
+        RefreshScoreText();
+    }
+    
+    void RefreshScoreText()
+    {
+        textScore.GetComponent<Text>().text = "徳：" + score + " / " + nextScore;
+    }
+}
---
+++ TheTemple/Assets/Scripts/OrbManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbManager : MonoBehaviour
+{
+    private GameObject gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("start");
+        gameManager = GameObject.Find("GameManager");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TouchOrb()
+    {
+        Debug.Log("touch begin");
+        if (Input.GetMouseButton(0) == false)
+        {
+            Debug.Log("return");
+            return;
+        }
+        Debug.Log("touch");
+
+        gameManager.GetComponent<GameManager>().GetOrb();
+        Destroy(this.gameObject);
+    }
+}
